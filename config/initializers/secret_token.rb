4492be7ff258e4643d878a8b46b0b268245fada3
Aaron Gray <aaronsallright@gmail.com>
2013-02-20 19:37:24 -0700

    Fresh app with  git ignore, rspec install, and fresh bundle.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '37f54a025c257543f7c45b503596ff67e7bc6ad8c9283eccdb64b00c6348284073ce1f907d9debe79af3993dd892d4a1e9960bb96865b4d6ad9dd2219e50c984'
